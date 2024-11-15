64258cb8f7d17e04879cae14862838498b4789af
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2024-11-15 19:12:51 +0100

    FIX cookie domain again

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,6 @@ import { getEnv } from "../common/utils/get-env";
 const appConfig = () => ({
   NODE_ENV: getEnv("NODE_ENV", "development"),
   APP_ORIGIN: getEnv("APP_ORIGIN", "localhost"),
-  COOKIE_DOMAIN: getEnv("COOKIE_DOMAIN", "localhost"),
   PORT: getEnv("PORT", "5000"),
   BASE_PATH: getEnv("BASE_PATH", "/api/v1"),
   MONGO_URI: getEnv("MONOGO_URI"),
