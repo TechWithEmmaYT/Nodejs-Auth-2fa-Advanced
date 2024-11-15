9c0ffe099f5ea053098b209342adb669fb26e287
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2024-11-15 12:01:26 +0100

    Fix server start issue

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.get(
   `/`,
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     res.status(HTTPSTATUS.OK).json({
-      message: "Hello Subscribers!!!",
+      message: "Hello Subscribers!!!!",
     });
   })
 );
